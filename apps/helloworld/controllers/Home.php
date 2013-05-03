d635f19769916f5bff96b4d8248983f6770aeb53
Aymeric ASSIER <a.assier@newquest.fr>
2013-05-03 16:35:40 +0200

    Add url example for Home::index in HelloWorld app

--- apps/helloworld/controllers/Home.php
+++ apps/helloworld/controllers/Home.php
@@ -5,6 +5,11 @@ namespace app\controllers;
 class Home
 {
 
+    /**
+     * www.you.com/helloworld/
+     * www.you.com/helloworld/home
+     * or www.you.com/helloworld/home/index
+     */
     public function index()
     {
         return ['name' => 'world'];
