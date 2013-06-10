89e8a919d5423ee9ba35bb53d39e8080a802c11d
Aymeric Assier <aymeric.assier@gmail.com>
2013-06-10 22:09:46 +0200

    Add relation support to syn's models (need multiname annotation support)

---
+++ wicked/tools/meta/Model.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace wicked\tools\meta;
+
+use wicked\tools\meta\Annotation;
+
+trait Model
+{
+
+    protected $__rel = [];
+
+    /**
+     * Find relation
+     * @param $entity
+     * @param array $where
+     * @return bool|\PDOStatement
+     * @throws \InvalidArgumentException
+     */
+    public function __call($entity, $where = [])
+    {
+        // lazy mapping
+        if(!$this->__rel) {
+
+            // get annotations
+            $annotations = Annotation::object($this, 'has');
+
+            // parse
+            if($annotations) {
+                foreach($annotations as $annotation) {
+                    @list($type, $name, $link) = explode(' ', $annotation);
+                    $link = trim($link, '()') ?: strtolower($name);
+                    $this->__rel[strtolower($name)] = (object)['type' => $type, 'link' => $link];
+                }
+            }
+
+        }
+
+        // clean
+        $entity = strtolower($entity);
+
+        // find relation
+        if(isset($this->__rel[$entity]) and $rel = $this->__rel[$entity]) {
+
+            // many
+            if($rel->type == 'many') {
+                return syn()->{$entity}->find([$rel->link => $this->id]);
+            }
+            elseif($rel->type == 'one') {
+                return syn()->{$entity}->find($rel->link);
+            }
+            else
+                throw new \InvalidArgumentException('Unknown relation type "' . $rel->type . '" in ' . get_called_class());
+        }
+        else
+            throw new \InvalidArgumentException('Method ' . $entity . ' does not exists in ' . get_called_class());
+    }
+
+}
